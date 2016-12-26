3c2d632b95b22e498e234dbbffe19b2e0f8c1d6e
Salnikov Sergey <ncr98@yandex.ru>
2016-12-26 19:18:35 +0300

    Add simple init script

---
+++ init.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -e
+docker-compose up -d
+echo 'Sleeping 10s - don`t forget KISS principle'
+sleep 10
+docker-compose exec tile /usr/local/sbin/run initdb startdb createuser createdb
+docker-compose exec tile sh -c 'sudo -upostgres psql -d gis -c "TRUNCATE spatial_ref_sys"'
+docker-compose exec tile sh -c 'sudo -upostgres psql -d gis -f /usr/share/postgresql/9.3/contrib/postgis-2.1/spatial_ref_sys.sql'
+docker-compose exec tile /usr/local/sbin/run import
+docker-compose restart
+echo 'Wait a bit & check tiles' 
