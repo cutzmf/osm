8a27c1e921f6b423e9cc03d8f2011cf9c3c4e4f7
Salnikov Sergey <ncr98@yandex.ru>
2017-02-27 17:49:21 +0300

    Add comment about data import

--- init.sh
+++ init.sh
@@ -6,6 +6,7 @@ sleep 10
 docker-compose exec tile /usr/local/sbin/run initdb startdb createuser createdb
 docker-compose exec tile sh -c 'sudo -upostgres psql -d gis -c "TRUNCATE spatial_ref_sys"'
 docker-compose exec tile sh -c 'sudo -upostgres psql -d gis -f /usr/share/postgresql/9.3/contrib/postgis-2.1/spatial_ref_sys.sql'
+# import file naming MUST be import.osm
 docker-compose exec tile /usr/local/sbin/run import
 docker-compose restart
 echo 'Wait a bit & check tiles' 
